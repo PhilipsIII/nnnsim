5b24ce6b0962291c26709b41e592117d4df95632
Jairo Eduardo Lopez <jelfn@sislacom.com>
2015-05-29 22:57:25 +0900

    Integration main 3N Wire headers

---
+++ model/nnn-nnnsim-icn-wire.h
@@ -0,0 +1,29 @@
+/* -*- Mode:C++; c-file-style:"gnu" -*- */
+/*
+ * Copyright (c) 2015 Jairo Eduardo Lopez
+ *
+ *   This file is part of nnnsim.
+ *
+ *  nnn-nnnsim-icn-wire.h is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nnn-nnnsim-icn-wire.h is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with nnn-nnnsim-icn-wire.h. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
+ */
+#ifndef NNN_NNNSIM_ICN_WIRE_H_
+#define NNN_NNNSIM_ICN_WIRE_H_
+
+#include "wire/icn-wire.h"
+#include "wire/wire-nnnsim-icn.h"
+#include "wire/nnnsim/nnnsim-icn.h"
+
+#endif
